6336f101ae06a294f5e3c5eafdbf2660192e369f
unknown <guii_arantes@hotmail.com>
2024-05-02 11:40:16 -0300

    css changes e add pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -21,18 +21,35 @@ const Pagination = () => {
     getChars();
   }, []);
 
-  
   return (
     <div className="navConteiner">
-       {/* <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-      /> */}
+      <nav aria-label="navPag">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link"><i class="bi bi-arrow-left"></i></a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              <i class="bi bi-arrow-right"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
